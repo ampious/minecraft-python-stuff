d27029326f209637c43831348c1b1011112a17fc
Adrian Plummer <a.plummer@unsw.edu.au>
2018-09-15 20:19:24 +1000

    Added pre-set locations

--- LearnToProgamWithMinecraft/Chapter2/teleporting-tutorial-1.py
+++ LearnToProgamWithMinecraft/Chapter2/teleporting-tutorial-1.py
@@ -2,11 +2,18 @@ from mcpi.minecraft import Minecraft
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--location", help="Enter a pre-defined location")
+parser.add_argument("--location", help="Enter a pre-defined location", choices = ['guest', 'oped'])
 parser.add_argument("--coords", default='10,110,12', help="Coordinates in form 'x,y,z'")
+parser.add_argument("--player", default='ampious', help="Set name of player to execute commands onto")
+parser.add_argument("--getpos", help="Get coordinates of selected player")
 #parser.add_argument("--y", help="Y-Coordinate")
 #parser.add_argument("--x", help="Z-Coordinate")
 args = parser.parse_args()        
+
+locations = {}
+locations['spawn_points'] = {}
+locations['spawn_points']['guest'] = [998,2,952]
+locations['spawn_points']['oped'] = [20,5,6]
 
 mc = Minecraft.create()
 
@@ -19,6 +26,15 @@ if args.coords != '10,110,12':
     x = coordinates[0]
     y = coordinates[1]
     z = coordinates[2]
+elif args.location:
+    if args.location == 'guest':
+        x = locations['spawn_points']['guest'][0]
+        y = locations['spawn_points']['guest'][1]
+        z = locations['spawn_points']['guest'][2]
+    elif args.location == 'oped':
+        x = locations['spawn_points']['oped'][0]
+        y = locations['spawn_points']['oped'][1]
+        z = locations['spawn_points']['oped'][2]
 else:
     x = 10
     y = 110
